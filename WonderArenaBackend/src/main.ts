16811630bcab9bcb66f140f73b561267e072411e
lauuurars <1120098177@u.icesi.edu.co>
2026-05-04 12:54:28 -0500

    i tried :c

--- WonderArenaBackend/src/main.ts
+++ WonderArenaBackend/src/main.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express"
 import { createServer } from "http"
+import socketio from "socket.io"
 import cors from "cors"
 import "dotenv/config"
 
 import { AuthRouter } from "./routes/auth/auth.router"
 import { GameRouter } from "./routes/game/game.router"
+import { addPlayer, getState, removePlayer } from "./game/arena"
 
 const app = express()
 app.use(cors())
@@ -23,3 +25,31 @@ rawServer.listen(8080, () => {
     console.log("Server running on port 8080")
 })
 
+const io = new socketio.Server(rawServer, {
+    path: "/real-time",
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("connected:", socket.id)
+
+    addPlayer(socket.id)
+
+    io.emit("game-update", {
+        Players: getState()
+    })
+
+    socket.on("player-move", ( { Payload: dx, dy }) => {
+
+    })
+
+    socket.on("player-eliminated", ({  id = socket.id }) => {
+        removePlayer(socket.id)
+
+        io.emit("game-update", {
+            Players: getState()
+        })
+    })
+})  
